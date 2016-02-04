830cdc4518d232ef6da10c0ccc1b49030dbe6c5a
Gwendolyn Faraday <gwenfaraday@gmail.com>
2016-02-04 12:31:26 -0500

    gulpfile setup and sass/compass

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
 
 
@@ -20,4 +21,17 @@ gulp.task('js', function() {
     .pipe(concat('script.js'))
     .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'));
+});
+
+var sassSources = ['components/sass/style.scss'];
+
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass',
+      image: 'builds/development/images',
+      style: 'expanded'
+    }))
+    .on('error', gutil.log) //outputs errors to console
+    .pipe(gulp.dest('builds/development/css'));
 });
